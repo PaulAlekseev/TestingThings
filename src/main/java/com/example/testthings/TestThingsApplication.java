bafc0239d0359b3410c2656e49fe7691f5ce8dde
Pavel <pavel.zommerfeld@yandex.ru>
2024-08-15 23:55:23 +0300

    init

---
+++ src/main/java/com/example/testthings/TestThingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testthings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestThingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestThingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testthings/TestThingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testthings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestThingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
